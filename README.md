fef2a553eef8307b7c65e3cf65b841a0d9d14b37
zoomina <seolhwa8191@gmail.com>
2020-11-10 13:36:03 +0900

    update load_data to json2csv

--- README.md
+++
@@ -1,3 +0,0 @@
-# MZ_CEC
-
-MZ CEC 감정대화분류 해커톤 emo:)tale팀 : 박건우, 변자민
---
+++ json2csv.py
@@ -0,0 +1,26 @@
+import json
+import argparse
+import os
+
+import pandas as pd
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data")
+    parser.add_argument("--output-dir", required=True)
+    parser.add_argument("--output-name", required=True)
+    args = parser.parse_args()
+
+    os.makedirs(args.output_dir, exist_ok=True)
+
+    df = pd.DataFrame(columns=["emotion_u", "emotion_i", "content"])
+
+    with open(args.data, "r", encoding="utf-8") as file:
+        root = next(file).strip()
+        for line in file:
+            line = eval(line[:-2])  # keys = profile, talk
+            profile_info = line["profile"]  # keys = persona-id, persona, emotion
+            talk_info = line["talk"]  # keys = id, content
+            inner = {"emotion_u": profile_info["emotion"]["type"][1], "emotion_i": profile_info["emotion"]["type"],
+                     "content": talk_info["content"]}
+            df = df.append(inner, ignore_index=True)
