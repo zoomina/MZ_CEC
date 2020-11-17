05db4c5a10e7f9240adb15e1cc7e1241d6d1b5bf
zoomina <seolhwa8191@gmail.com>
2020-11-17 16:00:55 +0900

    edit test
    >> return result(label:pred) dataframe

--- test.py
+++ test.py
@@ -12,6 +12,7 @@ def calc_accuracy(X,Y):
 def test(test_dataloader, model, device):
     model.eval()
     answer=[]
+    labels=[]
     test_acc = 0.0
     with torch.no_grad():
         for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(tqdm(test_dataloader)):
@@ -21,12 +22,16 @@ def test(test_dataloader, model, device):
             label = label.long().to(device)
             out = model(token_ids, valid_length, segment_ids)
             max_vals, max_indices = torch.max(out, 1)
-            answer.append(max_indices.cpu().clone().numpy())
+            answer.extend(max_indices.cpu().clone().numpy())
             test_acc += calc_accuracy(out, label)
+            labels.extend(label.cpu().clone().numpy())
     print(test_acc / (batch_id+1))
+    labels = np.array(labels).flatten()
+    preds = np.array(answer).flatten()
+    result = pd.DataFrame({"label": labels, "pred": preds})
+    return result
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser()
     parser.add_argument("checkpoint")
     parser.add_argument("--batch-size", type=int, default=64, help="default=64")
@@ -38,16 +43,17 @@ if __name__ == "__main__":
     num_workers = args.num_workers
     checkpoint = torch.load(args.checkpoint)
     '''
+
     batch_size = 24
     num_workers = 0
     checkpoint = torch.load("result/epoch3_batch24.pt")
     '''
     max_len = 64
-    model = BERTClassifier(num_classes=args.num_classes).build_model()
+    model = BERTClassifier().build_model()
     model.load_state_dict(checkpoint)
     device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
     _, eval_dtls = preprocessing()
 
     data_test = test_loader(eval_dtls, max_len, batch_size, num_workers)
 
-    test(data_test, model, device)
+    result_df = test(data_test, model, device)
